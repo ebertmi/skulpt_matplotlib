991877e04eac98f030afa7cca163312b72e40b7f
Michael Ebert <michael.ebert@gmail.com>
2015-05-04 16:32:38 +0200

    Simple algorithm for width/height ratio guessing #1

--- matplotlib/pyplot/__init__.js
+++ matplotlib/pyplot/__init__.js
@@ -1,3 +1,159 @@
+(function() {
+  var out$ = typeof exports != 'undefined' && exports || this;
+
+  var doctype = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
+
+  function isExternal(url) {
+    return url && url.lastIndexOf('http',0) == 0 && url.lastIndexOf(window.location.host) == -1;
+  }
+
+  function inlineImages(el, callback) {
+    var images = el.querySelectorAll('image');
+    var left = images.length;
+    if (left == 0) {
+      callback();
+    }
+    for (var i = 0; i < images.length; i++) {
+      (function(image) {
+        var href = image.getAttribute('xlink:href');
+        if (href) {
+          if (isExternal(href.value)) {
+            console.warn("Cannot render embedded images linking to external hosts: "+href.value);
+            return;
+          }
+        }
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        var img = new Image();
+        href = href || image.getAttribute('href');
+        img.src = href;
+        img.onload = function() {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          ctx.drawImage(img, 0, 0);
+          image.setAttribute('xlink:href', canvas.toDataURL('image/png'));
+          left--;
+          if (left == 0) {
+            callback();
+          }
+        };
+        img.onerror = function() {
+          console.log("Could not load "+href);
+          left--;
+          if (left == 0) {
+            callback();
+          }
+        };
+      })(images[i]);
+    }
+  }
+
+  function styles(el, selectorRemap) {
+    var css = "";
+    var sheets = document.styleSheets;
+    for (var i = 0; i < sheets.length; i++) {
+      if (isExternal(sheets[i].href)) {
+        console.warn("Cannot include styles from other hosts: "+sheets[i].href);
+        continue;
+      }
+      var rules = sheets[i].cssRules;
+      if (rules != null) {
+        for (var j = 0; j < rules.length; j++) {
+          var rule = rules[j];
+          if (typeof(rule.style) != "undefined") {
+            var match = null;
+            try {
+              match = el.querySelector(rule.selectorText);
+            } catch(err) {
+              console.warn('Invalid CSS selector "' + rule.selectorText + '"', err);
+            }
+            if (match) {
+              var selector = selectorRemap ? selectorRemap(rule.selectorText) : rule.selectorText;
+              css += selector + " { " + rule.style.cssText + " }\n";
+            } else if(rule.cssText.match(/^@font-face/)) {
+              css += rule.cssText + '\n';
+            }
+          }
+        }
+      }
+    }
+    return css;
+  }
+
+  out$.svgAsDataUri = function(el, options, cb) {
+    options = options || {};
+    options.scale = options.scale || 1;
+    var xmlns = "http://www.w3.org/2000/xmlns/";
+
+    inlineImages(el, function() {
+      var outer = document.createElement("div");
+      var clone = el.cloneNode(true);
+      var width, height;
+      if(el.tagName == 'svg') {
+        var box = el.getBoundingClientRect();
+        width = box.width;
+        height = box.height;
+      } else {
+        var box = el.getBBox();
+        width = box.x + box.width;
+        height = box.y + box.height;
+        clone.setAttribute('transform', clone.getAttribute('transform').replace(/translate\(.*?\)/, ''));
+
+        var svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
+        svg.appendChild(clone);
+        clone = svg;
+      }
+
+      clone.setAttribute("version", "1.1");
+      clone.setAttributeNS(xmlns, "xmlns", "http://www.w3.org/2000/svg");
+      clone.setAttributeNS(xmlns, "xmlns:xlink", "http://www.w3.org/1999/xlink");
+      clone.setAttribute("width", width * options.scale);
+      clone.setAttribute("height", height * options.scale);
+      clone.setAttribute("viewBox", "0 0 " + width + " " + height);
+      outer.appendChild(clone);
+
+      var css = styles(el, options.selectorRemap);
+      var s = document.createElement('style');
+      s.setAttribute('type', 'text/css');
+      s.innerHTML = "<![CDATA[\n" + css + "\n]]>";
+      var defs = document.createElement('defs');
+      defs.appendChild(s);
+      clone.insertBefore(defs, clone.firstChild);
+
+      var svg = doctype + outer.innerHTML;
+      var uri = 'data:image/svg+xml;base64,' + window.btoa(unescape(encodeURIComponent(svg)));
+      if (cb) {
+        cb(uri);
+      }
+    });
+  };
+
+  out$.saveSvgAsPng = function(el, name, options) {
+    options = options || {};
+    out$.svgAsDataUri(el, options, function(uri) {
+      var image = new Image();
+      image.src = uri;
+      image.onload = function() {
+        var canvas = document.createElement('canvas');
+        canvas.width = image.width;
+        canvas.height = image.height;
+        var context = canvas.getContext('2d');
+        context.drawImage(image, 0, 0);
+
+        var a = document.createElement('a');
+        a.download = name;
+        a.href = canvas.toDataURL('image/png');
+        document.body.appendChild(a);
+        a.addEventListener("click", function(e) {
+          a.parentNode.removeChild(a);
+        });
+        a.click();
+      };
+    });
+  };
+})();
+
+
 /**
 	Made by Michael Ebert for skulpt, see this modules at https://github.com/waywaaard/skulpt
 
@@ -620,14 +776,20 @@ jsplotlib.plot = function(chart) {
       this._lines.push(line);
       line._line_id = this.line_count++;
       this._update_limits();
+      this._update_chart_ratio();
     }
 
     return this;
   };
 
+  // calculate width-height-ratio
+
+
   // set graph attributes
   that._chartheight = parseInt(chart.attr("height"), 10);
   that._chartwidth = parseInt(chart.attr("width"));
+
+
   that._title_string = "";
   that._title_size = 0;
   that._xaxis = jsplotlib.construct_axis(that, "x");
@@ -719,6 +881,34 @@ jsplotlib.plot = function(chart) {
     this._ylimits([d3.min(ys), d3.max(ys)]);
 
     return this;
+  };
+
+  that._update_chart_ratio = function() {
+      var _height = chart.attr("height");
+      var _width = chart.attr("width");
+      var i;
+      var xs = []; // all x-values
+      var ys = []; // all y-values
+
+      // calculate limits
+      for (i = 0; i < this._lines.length; i++) {
+        xs = xs.concat(this._lines[i]._x);
+        ys = ys.concat(this._lines[i]._y);
+      }
+
+      var _abs_height = Math.abs(d3.min(xs) - d3.max(xs));
+      var _abs_width = Math.abs(d3.min(ys) - d3.max(ys));
+
+      var w_ratio = _abs_height / _abs_width;
+      var h_ratio = _abs_width / _abs_height;
+
+      h_ratio = h_ratio > 1 ? 1.25 : h_ratio < 1 ? 0.85 : 1;
+      w_ratio = w_ratio > 1 ? 1.25 : w_ratio < 1 ? 0.85 : 1;
+
+      that._chartheight = _height * h_ratio;
+      that._chartwidth = _width * w_ratio;
+      chart.attr("height", that._chartheight);
+      chart.attr("width", that._chartwidth);
   };
 
   that.yformat = function(formatter) {
@@ -1606,7 +1796,8 @@ var $builtinmodule = function(name) {
 
     if (!chart) {
       $('#' + Sk.canvas).empty();
-      chart = jsplotlib.make_chart(400, 400, "#" + Sk.canvas);
+      // min height and width
+      chart = jsplotlib.make_chart(450, 450, "#" + Sk.canvas);
     }
   };
 
@@ -1736,7 +1927,41 @@ var $builtinmodule = function(name) {
         "Can not call show without any plot created.");
     }
 
-    $('#' + Sk.canvas).show();
+    var $div_canvas =  $('#' + Sk.canvas);
+    $div_canvas.show();
+
+    //saveSvgAsPng(document.querySelector('#' + Sk.canvas + " svg"), "plot.png");
+
+    // add automatically a download link for the picture
+    /*
+    debugger;
+    var svg = document.querySelector('#' + Sk.canvas + " svg");
+    var svgData = new XMLSerializer().serializeToString( svg );
+     
+    var canvas = document.createElement("canvas");
+    var svgSize = svg.getBBox != null ? svg.getBBox() : svg.getBoundingClientRect();
+    canvas.width = svgSize.width;
+    canvas.height = svgSize.height;
+
+    canvas.width = $div_canvas.width();
+    canvas.height = $div_canvas.height();
+
+    var ctx = canvas.getContext("2d"); 
+    var img = document.createElement("img");
+    img.setAttribute("src", "data:image/svg+xml;base64," + btoa( svgData ));
+     
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+        
+        // Now is done
+        var canvasdata = canvas.toDataURL("image/png" );
+        var a = document.createElement("a");
+        a.download = "plot_"+Date.now()+".png";
+        a.href = canvasdata; 
+        document.body.appendChild(a);
+        a.click();
+    };
+    */
   };
   mod.show = new Sk.builtin.func(show_f);
 
